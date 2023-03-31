d0b47a360955adfcee69fb72b83b108cdc40605b
rohanasif1990 <rohanasif1990@gmail.com>
2023-03-31 23:13:23 +0500

    half questions done

---
+++ assignment11/index.js
@@ -0,0 +1,25 @@
+// q1
+// b = document.getElementById("heading");
+// b.innerText = "blah";
+// q2
+// a = document.getElementById("heading");
+// a.style.color = "red";
+// q3
+// a = document.getElementById("heading");
+// a.classList.add("mystyle");
+// q4
+// heading = document.getElementById("heading");
+// function toggleVisibility(){
+//     heading.classList.toggle("hidden")
+// }
+// q5
+// const para = document.createElement("p");
+// const node = document.createTextNode("This is new.");
+// para.appendChild(node);
+// const element = document.getElementById("div1");
+// element.appendChild(para);
+// q6
+// btn = document.getElementById("toggle-button");
+// btn.addEventListener("click", () => {
+//     btn.classList.add("blue");
+// })
