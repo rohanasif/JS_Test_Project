deaf07b9a27b39d6d25add747f5f927de0ad39c8
rohanasif1990 <rohanasif1990@gmail.com>
2023-04-04 22:03:16 +0500

    more assignments done

---
+++ assignment12/index.js
@@ -0,0 +1,17 @@
+function calculate(num1, num2, operator) {
+    if (operator === "+") {
+        return num1 + num2;
+    }
+    else if (operator === "-") {
+        return num1 - num2;
+    }
+    else if (operator === "*") {
+        return num1 * num2;
+    }
+    else if (operator === "/") {
+        return num1 / num2;
+    }
+    else {
+        return "Invalid operators"
+    }
+}
