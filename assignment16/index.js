33c74be5fe538553523115c65aeac3dba3bc2295
rohanasif1990 <rohanasif1990@gmail.com>
2023-04-06 16:28:22 +0500

    assignment 16 done

---
+++ assignment16/index.js
@@ -0,0 +1,6 @@
+// With JS addEventListener
+const btn = document.getElementById("btn");
+const p = document.getElementById("p");
+btn.addEventListener("click", () => {
+    p.innerHTML = "Rohan";
+})
