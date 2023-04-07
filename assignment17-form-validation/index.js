15cd1f899fe52bf76f4188511bd377d0064cfec3
rohanasif1990 <rohanasif1990@gmail.com>
2023-04-07 16:02:27 +0500

    almost complete

--- assignment17-form-validation/index.js
+++ assignment17-form-validation/index.js
@@ -5,6 +5,7 @@ const email = document.getElementById("email");
 const password = document.getElementById("password");
 const confirmation = document.getElementById("confirmation");
 const submitBtn = document.getElementById("submit-btn")
+var charCount = 0;
 
 submitBtn.addEventListener("click", (e) => {
     e.preventDefault();
@@ -15,5 +16,30 @@ submitBtn.addEventListener("click", (e) => {
             alert("Name should only be letters");
             break;
         }
+    }
+    const usernameValue = username.value;
+    if (usernameValue.length < 7) {
+        alert("Username should be atleast 7 characters long")
+    }
+    const emailValue = email.value;
+    if (!emailValue.includes("@")) {
+        alert("Invalid email")
+    }
+    const passwordValue = password.value;
+    const confirmationValue = confirmation.value;
+    if (passwordValue.length < 8) {
+        alert("Password should be atleast 8 characters long")
+    }
+    if (passwordValue[0] < "A" || passwordValue[0] > "Z") {
+        alert("First letter of password must be capital")
+    }
+    for (char of passwordValue) {
+        if (char) {
+            charCount++;
+        }
+    }
+
+    if (passwordValue !== confirmationValue) {
+        alert("Passwords do not match")
     }
 })
