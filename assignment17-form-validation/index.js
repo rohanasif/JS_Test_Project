015ffe35779ba1a1e9c9cc9abcccbe150b98ad42
rohanasif1990 <rohanasif1990@gmail.com>
2023-04-07 06:25:41 +0500

    name validation done

---
+++ assignment17-form-validation/index.js
@@ -0,0 +1,17 @@
+// Get all form fields and btn
+const name = document.getElementById("name");
+const username = document.getElementById("username");
+const email = document.getElementById("email");
+const password = document.getElementById("password");
+const confirmation = document.getElementById("confirmation");
+const submitBtn = document.getElementById("submit-btn");
+submitBtn.addEventListener("click", (e) => {
+    e.preventDefault();
+    const nameValue = name.value.toLowerCase();
+    for (i = 0; i < nameValue.length; i++) {
+        if ((nameValue[i] < "a" || nameValue[i] > "z") && nameValue[i] !== " ") {
+            alert("Name should only be letters");
+            break;
+        }
+    }
+})
