f8d28cd839832c42cdff585334c804e5b8142ae9
rohanasif1990 <rohanasif1990@gmail.com>
2023-04-07 06:26:40 +0500

    add a comment

--- assignment17-form-validation/index.js
+++ assignment17-form-validation/index.js
@@ -4,11 +4,13 @@ const username = document.getElementById("username");
 const email = document.getElementById("email");
 const password = document.getElementById("password");
 const confirmation = document.getElementById("confirmation");
-const submitBtn = document.getElementById("submit-btn");
+const submitBtn = document.getElementById("submit-btn")
+
 submitBtn.addEventListener("click", (e) => {
     e.preventDefault();
     const nameValue = name.value.toLowerCase();
     for (i = 0; i < nameValue.length; i++) {
+        // If is not alphabet
         if ((nameValue[i] < "a" || nameValue[i] > "z") && nameValue[i] !== " ") {
             alert("Name should only be letters");
             break;
