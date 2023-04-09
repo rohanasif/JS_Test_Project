6820bd806cc1a54e5e25fcef56c65ff6aa060908
rohanasif1990 <rohanasif1990@gmail.com>
2023-04-10 01:04:51 +0500

    table gets the data even with validation

--- assignment18-inputtotable/index.js
+++ assignment18-inputtotable/index.js
@@ -3,10 +3,47 @@ const make = document.getElementById("make");
 const model = document.getElementById("model");
 const reg = document.getElementById("reg");
 const color = document.getElementById("color");
+
+const makecheck = document.getElementById("makecheck");
+const modelcheck = document.getElementById("modelcheck");
+const regcheck = document.getElementById("regcheck");
+const colorcheck = document.getElementById("colorcheck");
+
 const submitbtn = document.getElementById("submit-btn");
+
+make.addEventListener("input", (e) => {
+    const makeValue = make.value;
+    if (makeValue.length < 8) {
+        makecheck.innerText = "Car make should be atleast 8 characters long";
+    }
+    else {
+        makecheck.innerText = "";
+    }
+})
+
+model.addEventListener("input", (e) => {
+    const modelValue = model.value;
+    if (modelValue.length < 8) {
+        modelcheck.innerText = "Car model should be atleast 8 characters long";
+    }
+    else {
+        modelcheck.innerText = "";
+    }
+})
+
+reg.addEventListener("input", (e) => {
+    const regValue = reg.value;
+    if (regValue.includes("0") || regValue.includes("1") || regValue.includes("2") || regValue.includes("3") || regValue.includes("4") || regValue.includes("5") || regValue.includes("6") || regValue.includes("7") || regValue.includes("8") || regValue.includes("9")) {
+        regcheck.innerText = "";
+    }
+    else {
+        regcheck.innerText = "Car registration should contain numbers";
+    }
+})
 
 submitbtn.addEventListener("click", (e) => {
     e.preventDefault();
+
     // Get all values from the form
     const makeValue = make.value;
     const modelValue = model.value;
